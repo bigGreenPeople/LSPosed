08755c8229b9ee9ab3cf26a3bfad07569a96cdce
bigGreenPeople <1243596620@qq.com>
2023-08-14 16:14:02 +0800

    分析

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,16 +54,23 @@ cmaker {
     }
 }
 
-val repo = jgit.repo()
+// 定义 Git 仓库信息的变量
+val repo = jgit.repo()  // 初始化一个 Git 仓库对象
+// 计算提交数量
 val commitCount = (repo?.commitCount("refs/remotes/origin/master") ?: 1) + 4200
+// 从仓库获取最新的标签或使用默认值
 val latestTag = repo?.latestTag?.removePrefix("v") ?: "1.0"
 
+// 定义注入包信息的变量
 val injectedPackageName by extra("com.android.shell")
 val injectedPackageUid by extra(2000)
 
+// 定义默认管理器包信息的变量
 val defaultManagerPackageName by extra("org.lsposed.manager")
 val verCode by extra(commitCount)
 val verName by extra(latestTag)
+
+//下面都是写sdk的版本信息
 val androidTargetSdkVersion by extra(34)
 val androidMinSdkVersion by extra(27)
 val androidBuildToolsVersion by extra("34.0.0")
@@ -72,13 +79,17 @@ val androidCompileNdkVersion by extra("25.2.9519653")
 val androidSourceCompatibility by extra(JavaVersion.VERSION_17)
 val androidTargetCompatibility by extra(JavaVersion.VERSION_17)
 
+// 注册一个任务来删除 build 目录
 tasks.register("Delete", Delete::class) {
     delete(rootProject.buildDir)
 }
 
 subprojects {
+    // 对于应用 Android 插件
     plugins.withType(AndroidBasePlugin::class.java) {
+        // 配置 CommonExtension，这是 Android 插件的一个扩展
         extensions.configure(CommonExtension::class.java) {
+            // 设置编译所需的 SDK 版本、NDK 版本、构建工具版本等
             compileSdk = androidCompileSdkVersion
             ndkVersion = androidCompileNdkVersion
             buildToolsVersion = androidBuildToolsVersion
@@ -89,6 +100,7 @@ subprojects {
                 }
             }
 
+            // 配置默认配置，包括最小支持 SDK 版本、目标 SDK 版本、版本号和版本名
             defaultConfig {
                 minSdk = androidMinSdkVersion
                 if (this is ApplicationDefaultConfig) {
@@ -98,19 +110,23 @@ subprojects {
                 }
             }
 
+            // 配置 Lint，设置是否在错误出现时中止构建，以及是否检查 Release 构建
             lint {
                 abortOnError = true
                 checkReleaseBuilds = false
             }
-
+            // 配置编译选项，设置源代码和目标兼容性
             compileOptions {
                 sourceCompatibility = androidSourceCompatibility
                 targetCompatibility = androidTargetCompatibility
             }
         }
     }
+    // 对于 Java 插件
     plugins.withType(JavaPlugin::class.java) {
+        // 配置 Java 插件扩展
         extensions.configure(JavaPluginExtension::class.java) {
+            // 设置源代码和目标兼容性
             sourceCompatibility = androidSourceCompatibility
             targetCompatibility = androidTargetCompatibility
         }
--- magisk-loader/build.gradle.kts
+++ magisk-loader/build.gradle.kts
@@ -127,19 +127,28 @@ val zipAll = task("zipAll") {
     group = "LSPosed"
 }
 
+// 定义一个函数，用于在构建配置评估后执行一些操作
 fun afterEval() = android.applicationVariants.forEach { variant ->
+    // 根据不同的变体（variant）、构建类型（buildType）、风味（flavor）生成不同的字符串格式
+    //ZygiskDebug
     val variantCapped = variant.name.replaceFirstChar { it.uppercase() }
+    //zygiskdebug
     val variantLowered = variant.name.lowercase()
     val buildTypeCapped = variant.buildType.name.replaceFirstChar { it.uppercase() }
+    //debug
     val buildTypeLowered = variant.buildType.name.lowercase()
     val flavorCapped = variant.flavorName!!.replaceFirstChar { it.uppercase() }
+    //zygisk
     val flavorLowered = variant.flavorName!!.lowercase()
 
+    //zygiskdebug
     val magiskDir = "$buildDir/magisk/$variantLowered"
 
     val moduleId = "${flavorLowered}_$moduleBaseId"
+    //LSPosed-v1.0-6964-zygisk-debug.zip
     val zipFileName = "$moduleName-v$verName-$verCode-${flavorLowered}-$buildTypeLowered.zip"
 
+    //动态创建task用于先编译相关项目 再打包到$buildDir/magisk/$variantLowered
     val prepareMagiskFilesTask = task<Sync>("prepareMagiskFiles$variantCapped") {
         group = "LSPosed"
         dependsOn(
@@ -192,6 +201,7 @@ fun afterEval() = android.applicationVariants.forEach { variant ->
                 filter<FixCrLfFilter>("eol" to FixCrLfFilter.CrLf.newInstance("lf"))
             }
         }
+        //获取名为 :app:package$buildTypeCapped 这个构建任务的输出目录。
         from(project(":app").tasks.getByName("package$buildTypeCapped").outputs) {
             include("*.apk")
             rename(".*\\.apk", "manager.apk")
@@ -200,16 +210,19 @@ fun afterEval() = android.applicationVariants.forEach { variant ->
             include("*.apk")
             rename(".*\\.apk", "daemon.apk")
         }
+        //拷贝zygisk 注入的so
         into("lib") {
             from("${buildDir}/intermediates/stripped_native_libs/$variantCapped/out/lib") {
                 include("**/liblspd.so")
             }
         }
+        //拷贝dex2oat 我们现在暂时没有用到
         into("bin") {
             from("${project(":dex2oat").buildDir}/intermediates/cmake/$buildTypeLowered/obj") {
                 include("**/dex2oat")
             }
         }
+        //拷贝需要加载的dex
         val dexOutPath = if (buildTypeLowered == "release")
             "$buildDir/intermediates/dex/$variantCapped/minify${variantCapped}WithR8" else
             "$buildDir/intermediates/dex/$variantCapped/mergeDex$variantCapped"
@@ -218,6 +231,7 @@ fun afterEval() = android.applicationVariants.forEach { variant ->
             rename("classes.dex", "lspd.dex")
         }
 
+        //计算 SHA-256
         val injected = objects.newInstance<Injected>(magiskDir)
         doLast {
             injected.factory.fileTree().from(injected.magiskDir).visit {
@@ -231,6 +245,7 @@ fun afterEval() = android.applicationVariants.forEach { variant ->
         }
     }
 
+    //动态创建task 打包具体zip
     val zipTask = task<Zip>("zip${variantCapped}") {
         group = "LSPosed"
         dependsOn(prepareMagiskFilesTask)
@@ -239,15 +254,26 @@ fun afterEval() = android.applicationVariants.forEach { variant ->
         from(magiskDir)
     }
 
+    //前面已经定义 这里动态添加zip task
     zipAll.dependsOn(zipTask)
 
+    /**
+     * 获取 Android SDK 工具中的 adb（Android Debug Bridge）工具的路径。
+     * androidComponents 是一个 Android Gradle 插件提供的对象，用于访问 Android 组件，如 SDK 组件、构建工具等。
+     * sdkComponents 是 androidComponents 中的一个属性，用于访问 SDK 相关的组件。
+     * adb 是 adb 工具的一个路径，通过调用 get() 方法来获取其位置。
+     * asFile 用于将 adb 工具的路径转换为文件对象。
+     * absolutePath 是获取文件对象的绝对路径。
+     */
     val adb: String = androidComponents.sdkComponents.adb.get().asFile.absolutePath
+    //动态创建task push
     val pushTask = task<Exec>("push${variantCapped}") {
         group = "LSPosed"
         dependsOn(zipTask)
         workingDir("${projectDir}/release")
         commandLine(adb, "push", zipFileName, "/data/local/tmp/")
     }
+    //使用命令刷入zip
     val flashTask = task<Exec>("flash${variantCapped}") {
         group = "LSPosed"
         dependsOn(pushTask)
@@ -256,6 +282,7 @@ fun afterEval() = android.applicationVariants.forEach { variant ->
             "magisk --install-module /data/local/tmp/${zipFileName}"
         )
     }
+    //刷入后重启
     task<Exec>("flashAndReboot${variantCapped}") {
         group = "LSPosed"
         dependsOn(flashTask)
@@ -263,22 +290,30 @@ fun afterEval() = android.applicationVariants.forEach { variant ->
     }
 }
 
+//在 Gradle 对项目的所有配置和依赖进行评估并准备好后执行。即动态生成上面的task
 afterEvaluate {
     afterEval()
 }
 
+//获取 Android SDK 工具中的 adb（Android Debug Bridge）工具的路径。
 val adb: String = androidComponents.sdkComponents.adb.get().asFile.absolutePath
+
+//杀死名为 lspd 的进程
 val killLspd = task<Exec>("killLspd") {
     group = "LSPosed"
     commandLine(adb, "shell", "su", "-c", "killall", "lspd")
     isIgnoreExitValue = true
 }
+
+//push daemon的java代码
 val pushDaemon = task<Exec>("pushDaemon") {
     group = "LSPosed"
     dependsOn(":daemon:assembleDebug")
     workingDir("${project(":daemon").buildDir}/outputs/apk/debug")
     commandLine(adb, "push", "daemon-debug.apk", "/data/local/tmp/daemon.apk")
 }
+
+//push daemon对应平台的so
 val pushDaemonNative = task<Exec>("pushDaemonNative") {
     group = "LSPosed"
     dependsOn(":daemon:assembleDebug")
@@ -294,6 +329,8 @@ val pushDaemonNative = task<Exec>("pushDaemonNative") {
     }
     commandLine(adb, "push", "libdaemon.so", "/data/local/tmp/libdaemon.so")
 }
+
+//重新启动daemon程序 这里直接执行了service.sh
 val reRunDaemon = task<Exec>("reRunDaemon") {
     group = "LSPosed"
     dependsOn(pushDaemon, pushDaemonNative, killLspd)
@@ -304,13 +341,17 @@ val reRunDaemon = task<Exec>("reRunDaemon") {
     )
     isIgnoreExitValue = true
 }
+
+
 val tmpApk = "/data/local/tmp/lsp.apk"
+//push 管理应用App
 val pushApk = task<Exec>("pushApk") {
     group = "LSPosed"
     dependsOn(":app:assembleDebug")
     workingDir("${project(":app").buildDir}/outputs/apk/debug")
     commandLine(adb, "push", "app-debug.apk", tmpApk)
 }
+//打开管理应用
 val openApp = task<Exec>("openApp") {
     group = "LSPosed"
     commandLine(
@@ -319,6 +360,8 @@ val openApp = task<Exec>("openApp") {
         "com.android.shell/.BugreportWarningActivity"
     )
 }
+
+//重启管理应用
 task<Exec>("reRunApp") {
     group = "LSPosed"
     dependsOn(pushApk)
@@ -327,5 +370,7 @@ task<Exec>("reRunApp") {
     finalizedBy(reRunDaemon)
 }
 
+//evaluationDependsOn 是 Gradle 构建脚本中的一个方法，
+//用于指定在评估项目时要依赖于哪些子项目的评估。它可以确保被依赖的项目在当前项目之前被评估。
 evaluationDependsOn(":app")
 evaluationDependsOn(":daemon")
